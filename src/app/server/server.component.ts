a8384fa529f042b59c4ebc783abcebeb9a45a2bb
Paul Thibodeaux <pthibodeaux@syncroness.com>
2017-10-25 10:08:36 -0600

    Update for 10/25

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = "offline_XXX";
 
+  constructor() {
+      console.log( Math.random());
+      this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  };
+  getColor(){
+    return serverStatus === 'online' ? 'green' : 'red';
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
+  // template: `<app-server></app-server>
+  // <app-server></app-server>`,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server created';
+  serverName = 'default_server_name';
+  serverCreated = false;
 
-  constructor() { }
+  constructor() {
+
+    setTimeout(  () => {this.allowNewServer = true;}, 2000 )
+  }
 
   ngOnInit() {
   }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+// also works
+  // onUpdateServerName(event: any) {
+  //   this.serverName = event.target.value;
+  // }
 
 }
